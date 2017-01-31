af92c00e522389a776db0156d3a6f3867a1f6b85
Tjitske Lohstroh <tjitske@lohstroh.nl>
2017-01-31 20:12:50 +0100

    make PostsIndex into container

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { fetchPosts } from '../actions/index';
 
 class PostsIndex extends Component {
-  // react will call this automactically whenever our component is ABOUT to be
+  // 80 react will call this automactically whenever our component is ABOUT to be
   // rendered to the DOM for the first time. It will be only called once!
   componentWillMount() {
     console.log('this would be a great time to call an action creator to fetch posts');
+    this.props.fetchPosts();
   }
 
   render(){
@@ -14,8 +18,12 @@ class PostsIndex extends Component {
   }
 }
 
-export default PostsIndex;
+function mapDispatchToProps(dispatch){
+  return bindActionCreators({ fetchPosts }, dispatch);
+}
 
-// export default () => {
-//   return <div>List of blogposts</div>;
-// };
+// first arg to a connect function is usually mapStateToProps
+// In this case we don't have that function (yet) so we'll pass in null.
+export default connect ( null, mapDispatchToProps)(PostsIndex);
+// This gives us acces to this.props.fetchPosts,
+// so we can now call it inside of component
