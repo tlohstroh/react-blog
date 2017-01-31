f2767dbbf67fa4412c4db328a9e0231368f8a896
Tjitske Lohstroh <tjitske@lohstroh.nl>
2017-01-31 19:16:24 +0100

    Add Lifecycle method to PostsIndex Component

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,5 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-export default () => {
-  return <div>List of blogposts</div>;
-};
+class PostsIndex extends Component {
+  // react will call this automactically whenever our component is ABOUT to be
+  // rendered to the DOM for the first time. It will be only called once!
+  componentWillMount() {
+    console.log('this would be a great time to call an action creator to fetch posts');
+  }
+
+  render(){
+    return (
+      <div>List of blogposts</div>
+    );
+  }
+}
+
+export default PostsIndex;
+
+// export default () => {
+//   return <div>List of blogposts</div>;
+// };
