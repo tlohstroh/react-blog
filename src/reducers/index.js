f56011ba103176b63c3761b9426c93ad6cb00366
Tjitske Lohstroh <tjitske@lohstroh.nl>
2017-02-01 10:14:01 +0100

    Install redux-form

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,13 @@
 import { combineReducers } from 'redux';
 import PostsReducer from './reducer_posts';
+// import redux-form, grab the reducer prop off of it
+// and create a variable with it named formReducer.
+import { reducer as formReducer } from 'redux-form';
 
 const rootReducer = combineReducers({
   // state: (state = {}) => state
-  posts: PostsReducer
+  posts: PostsReducer,
+  form: formReducer
 });
 
 export default rootReducer;
