acda9de08935a32152b41b0fa2425f0d5b822a9a
Tjitske Lohstroh <tjitske@lohstroh.nl>
2017-02-01 12:27:37 +0100

    form validation

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -16,20 +16,28 @@ class PostsNew extends Component {
       // So for instance onChange={title.onChange} becomes available..
       <form onSubmit={handleSubmit(this.props.createPost)}>
         <h3>Create A New Post</h3>
-        <div className="form-group">
+        <div className={`form-group ${title.touched && title.invalid ? 'has-danger' : ''}`}>
           <label>Title</label>
           <input type="text" className="form-control" {...title}/>
-          <div className="text-help">{title.touched ? title.error : ''}</div>
+          <div className="text-help">
+            {title.touched ? title.error : ''}
+          </div>
         </div>
 
-        <div className="form-group">
+        <div className={`form-group ${categories.touched && categories.invalid ? 'has-danger' : ''}`}>
           <label>Categories</label>
           <input type="text" className="form-control" {...categories}/>
+          <div className="text-help">
+            {categories.touched ? categories.error : ''}
+          </div>
         </div>
 
-        <div className="form-group">
+        <div className={`form-group ${content.touched && content.invalid ? 'has-danger' : ''}`}>
           <label>Content</label>
           <textarea className="form-control" {...content}/>
+          <div className="text-help">
+            {content.touched ? content.error : ''}
+          </div>
         </div>
 
         <button type="submit" className="btn btn-primary">Sumbit</button>
@@ -45,6 +53,13 @@ function validate(values){
   if (!values.title){
     errors.title = 'Enter a title';
   }
+  if (!values.categories){
+    errors.categories = 'Enter categories';
+  }
+  if (!values.content){
+    errors.content = 'Enter some content';
+  }
+
 
   return errors;
 }
