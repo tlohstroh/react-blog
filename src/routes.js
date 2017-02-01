42345587124c89b7aa64b868374e4639db015ad0
Tjitske Lohstroh <tjitske@lohstroh.nl>
2017-02-01 21:03:25 +0100

    Dynamic route params

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,7 @@ import { Route, IndexRoute } from 'react-router';
 import App from './components/app';
 import PostsIndex from './components/posts_index';
 import PostsNew from './components/posts_new';
+import PostsShow from './components/posts_show';
 
 // const Greeting = () => {
 //   return <div>Hey there!</div>;
@@ -15,5 +16,6 @@ export default (
     {/* if the route is "/" show app and show postIndex */}
     <IndexRoute component={PostsIndex} />
     <Route path="posts/new" component={PostsNew} />
+    <Route path="posts/:id" component={PostsShow} />
   </Route>
 );
